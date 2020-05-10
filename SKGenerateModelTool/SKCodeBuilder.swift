d67d35444bec34f9b20357d39e184b05d9bc9dfb
wushangkun <wushangkun@chelun.com>
2020-05-10 22:12:20 +0800

    add initial files

---
+++ SKGenerateModelTool/SKCodeBuilder.swift
@@ -0,0 +1,85 @@
+//
+//  SKCodeBuilder.swift
+//  SKGenerateModelTool
+//
+//  Created by KUN on 2020/5/10.
+//  Copyright © 2020 wushangkun. All rights reserved.
+//
+
+import Cocoa
+
+enum SKCodeBuilderCodeType: Int {
+    case OC = 1
+    case Swift
+    case Java
+}
+
+enum SKCodeBuilderJSONModelType: Int {
+    case None = 0
+    case YYModel
+    case MJExtension
+    case HandyJSON
+}
+
+typealias BuildComplete = (NSMutableString, NSMutableString) -> ()
+typealias GenerateFileComplete = (Bool, String) -> ()
+
+class SKCodeBuilder: NSObject {
+
+    var config = SKCodeBuilderConfig()
+    
+    func build_OC_code(with jsonObj:Any, complete:BuildComplete?){
+        let hString = NSMutableString()
+        let mString = NSMutableString()
+        handleDictValue(dictValue: jsonObj, key: "", hString: hString, mString: mString)
+        if let handler = complete  {
+            handler(hString, mString)
+        }
+    }
+    
+    func generate_OC_File(with filePath:String, hString:NSMutableString, mString:NSMutableString, complete:GenerateFileComplete) {
+        
+    }
+    
+    private func handleDictValue(dictValue:Any, key:String, hString:NSMutableString, mString:NSMutableString) {
+        
+        if key.isBlank { // Root model
+            let modeName = modelName(with: key)
+            hString.appending("\n\n@interface \(modeName) : \(self.config.superClassName)\n\n")
+            mString.appending("\n\n@implementation \(modeName)\n\n")
+
+        } else { // sub model
+            hString.appending("\n\n@interface \(self.config.rootModelName) : \(self.config.superClassName)\n\n")
+            mString.appending("\n\n@implementation \(self.config.rootModelName)\n\n")
+        }
+    }
+    
+    private func modelName(with key:String) -> String {
+        if key.isBlank { return config.rootModelName }
+        let firstCharacterIndex = key.index(key.startIndex, offsetBy: 1)
+        var firstCharacter = String(key[...firstCharacterIndex])
+        firstCharacter = firstCharacter.uppercased()
+        let start = String.Index.init(utf16Offset: 0, in: key)
+        let end = String.Index.init(utf16Offset: 1, in: key)
+        var modelName = key.replacingCharacters(in: start..<end, with: firstCharacter)
+        if !modelName.hasPrefix(config.modelNamePrefix) {
+            modelName = config.modelNamePrefix + key
+        }
+        return modelName
+    }
+}
+
+class SKCodeBuilderConfig: NSObject {
+    var superClassName = "NSObject"
+    var rootModelName = "NSRootModel"
+    var modelNamePrefix = "NS"
+    var codeType: SKCodeBuilderCodeType = .OC
+    var jsonType: SKCodeBuilderJSONModelType = .None
+}
+
+extension String {
+    var isBlank: Bool {
+        let trimmedStr = self.trimmingCharacters(in: .whitespacesAndNewlines)
+        return trimmedStr.isEmpty
+    }
+}
--- SKGenerateModelTool/ViewController.swift
+++ SKGenerateModelTool/ViewController.swift
@@ -9,16 +9,80 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var urlTF: NSTextField!
+    @IBOutlet weak var jsonTextView: NSTextView!
+    @IBOutlet weak var hTextView: NSTextView!
+    @IBOutlet weak var mTextView: NSTextView!
+    @IBOutlet weak var hTextViewHeightPriority: NSLayoutConstraint!
+    @IBOutlet weak var superClassNameTF: NSTextField!  /// default 3:5
+    @IBOutlet weak var modelNamePrefixTF: NSTextField!
+    @IBOutlet weak var rootModelNameTF: NSTextField!
+    @IBOutlet weak var authorNameTF: NSTextField!
+    @IBOutlet weak var reqTypeBtn: NSPopUpButton!
+    @IBOutlet weak var codeTypeBtn: NSPopUpButton!
+    @IBOutlet weak var jsonTypeBtn: NSPopUpButton!
+    @IBOutlet weak var generateFileBtn: NSButton!
+
+    var outputFilePath: String?
+
+    let LastInputURLCacheKey = "LastInputURLCacheKey"
+    let SuperClassNameCacheKey = "SuperClassNameCacheKey"
+    let RootModelNameCacheKey = "RootModelNameCacheKey"
+    let ModelNamePrefixCacheKey = "ModelNamePrefixCacheKey"
+    let AuthorNameCacheKey = "AuthorNameCacheKey"
+    let BuildCodeTypeCacheKey = "BuildCodeTypeCacheKey"
+    let SupportJSONModelTypeCacheKey = "SupportJSONModelTypeCacheKey"
+    let ShouldGenerateFileCacheKey = "ShouldGenerateFileCacheKey"
+    let GenerateFilePathCacheKey = "GenerateFilePathCacheKey"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        reqTypeBtn.removeAllItems()
+        reqTypeBtn.addItems(withTitles: ["GET","POST"])
+        reqTypeBtn.selectItem(at: 0)
+        
+        codeTypeBtn.removeAllItems()
+        codeTypeBtn.addItems(withTitles: ["Objective-C","Swift"])
+        codeTypeBtn.selectItem(at: 0)
 
-        // Do any additional setup after loading the view.
+        jsonTypeBtn.removeAllItems()
+        jsonTypeBtn.addItems(withTitles: ["None","YYMode","MJExtension","HandyJSON"])
+        jsonTypeBtn.selectItem(at: 0)
+        
     }
+    
+    override func viewDidAppear() {
+        loadUserLastInputContent()
+    }
+    
+    func loadUserLastInputContent() {
+        if let lastUrl = UserDefaults.standard.string(forKey: LastInputURLCacheKey)  {
+            urlTF.stringValue = lastUrl
+        }
+        if let superClassName = UserDefaults.standard.string(forKey: SuperClassNameCacheKey)  {
+            superClassNameTF.stringValue = superClassName
+        }
+        if let modelNamePrefix = UserDefaults.standard.string(forKey: ModelNamePrefixCacheKey)  {
+            modelNamePrefixTF.stringValue = modelNamePrefix
+        }
+        if let rootModelName = UserDefaults.standard.string(forKey: RootModelNameCacheKey)  {
+            rootModelNameTF.stringValue = rootModelName
+        }
+        if let authorName = UserDefaults.standard.string(forKey: AuthorNameCacheKey)  {
+            authorNameTF.stringValue = authorName
+        }
+        if let outFilePath = UserDefaults.standard.string(forKey: LastInputURLCacheKey)  {
+            outputFilePath = outFilePath
+        }
+        
+    }
+    
+    
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
 
