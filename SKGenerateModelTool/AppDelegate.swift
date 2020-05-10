698f433bc9c5316b7821bbaf1dfa9b06262c84a3
wushangkun <wushangkun@chelun.com>
2020-05-10 11:12:43 +0800

    Initial Commit

---
+++ SKGenerateModelTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SKGenerateModelTool
+//
+//  Created by KUN on 2020/5/10.
+//  Copyright © 2020 wushangkun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SKGenerateModelTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SKGenerateModelTool
+//
+//  Created by KUN on 2020/5/10.
+//  Copyright © 2020 wushangkun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
